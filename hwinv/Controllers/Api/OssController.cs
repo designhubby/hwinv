d4776acd119eb93037e369c073e5aeff9ce6f220
K L <kinglaugood@gmail.com>
2020-03-26 00:11:54 -0400

    adding control os

--- hwinv/Controllers/Api/OssController.cs
+++ hwinv/Controllers/Api/OssController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Data.Entity;
-using System.Net.Http;
-using AutoMapper;
-using System.Web.Http;
-using hwinv.Models;
+﻿using AutoMapper;
 using hwinv.Dtos;
+using hwinv.Models;
+using System;
+using System.Linq;
+using System.Web.Http;
+
 
 namespace hwinv.Controllers.Api
 {
@@ -21,6 +18,7 @@ namespace hwinv.Controllers.Api
         }
 
         // GET api/<controller>
+        [HttpGet]
         public IHttpActionResult GetOss(int id)
         {
             var item_os = _context.Os.SingleOrDefault(o => o.OsId == id);
@@ -34,7 +32,7 @@ namespace hwinv.Controllers.Api
 
             return Ok(item_os_dto);
         }
-
+        [HttpGet]
         // GET api/<controller>/5
         public IHttpActionResult GetOss(string name = null)
         {
@@ -49,10 +47,20 @@ namespace hwinv.Controllers.Api
 
             return Ok(list_os_dto) ;
         }
-
+        [HttpPost]
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public IHttpActionResult CreateOss(OsDto osDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var DB_Item_Os = Mapper.Map<OsDto, Os>(osDto);
+            _context.Os.Add(DB_Item_Os);
+            _context.SaveChanges();
+            osDto.OsId = DB_Item_Os.OsId;
+
+            return Created(new Uri(Request.RequestUri + "/" + osDto.OsId), osDto);
         }
 
         // PUT api/<controller>/5
